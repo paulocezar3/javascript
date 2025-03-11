3e796fb4ec4ac5c7855dc7fcd1577a77f22cb072
paulo cezar <paulocezarpazrocha7@gmail.com>
2025-03-11 10:38:16 -0300

    aula 11

---
+++ aula 11/ex08.js
@@ -0,0 +1,8 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Você ultrappasou a velocidade permitida. MULTADO!')
+}
+else{
+console.log('Dirija sempre usando cinto de segurança!')
+}
---
+++ aula 11/ex09.js
@@ -0,0 +1,8 @@
+var pais = 'EUa'
+console.log(`vivendo em ${pais}`)
+if (pais != 'brasil'){
+    console.log('Você é Estrangeiro ')
+}
+else{
+    console.log('Você é Brasileiro')
+}
